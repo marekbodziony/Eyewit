32f878528583a0abeb8efdce732bf5ad755ff18d
Marek Bodziony <marek_bodziony@o2.pl>
2017-10-14 19:43:14 +0200

    Send json to server

--- app/src/main/java/marekbodziony/eyewit/MainActivity.java
+++ app/src/main/java/marekbodziony/eyewit/MainActivity.java
@@ -4,24 +4,27 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.IOException;
 import java.util.Date;
 
+import marekbodziony.eyewit.api.Api;
 import marekbodziony.eyewit.model.Incident;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -29,6 +32,7 @@ public class MainActivity extends AppCompatActivity{
     private static final int PERMISSION_FINE_LOCATION_REQUEST = 1;
 
     private ImageButton recordBtn;
+    private Button policeBtn;
     private TextView clickTimesTextView;
     private TextView lat;
     private TextView lon;
@@ -36,6 +40,8 @@ public class MainActivity extends AppCompatActivity{
 
     private int clickTimes = 3;
     private Incident incident;
+
+    private Api api;
 
     private FusedLocationProviderClient locationProviderClient;
 
@@ -51,14 +57,16 @@ public class MainActivity extends AppCompatActivity{
         lat = (TextView) findViewById(R.id.lat_val);
         lon = (TextView) findViewById(R.id.lon_val);
         time = (TextView) findViewById(R.id.time_val);
+        policeBtn = (Button) findViewById(R.id.policja_btn);
 
         clickTimesTextView.setText(String.valueOf(clickTimes));
+
+        api = new Api();
 
         locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         askForLocationAndIfNeedForPermission();
 
         time.setText(new Date().toString());
-
 
         recordBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,6 +83,23 @@ public class MainActivity extends AppCompatActivity{
         });
 
         incident = new Incident();
+
+        policeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String response ="";
+                 {
+                     new Api.AsyncTask(api) {
+                         @Override
+                         protected void onPostExecute(String response) {
+                             super.onPostExecute(response);
+//                             Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                             Log.i("Marek",response);
+                         }
+                     }.execute("json");
+                 }
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/marekbodziony/eyewit/api/Api.java
@@ -0,0 +1,50 @@
+package marekbodziony.eyewit.api;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+public class Api {
+
+    private static final String SERVER_URL = "http://192.168.43.76:8081/home/";
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    OkHttpClient client = new OkHttpClient();
+
+    public String post (String json) throws IOException{
+        RequestBody requestBody = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(SERVER_URL)
+                .post(requestBody)
+                .build();
+        try (Response response = client.newCall(request).execute()){
+            return response.body().string();
+        }
+    }
+
+    public static class AsyncTask extends android.os.AsyncTask<String,Void,String>{
+
+        Api api;
+
+        public AsyncTask(Api api) {
+            this.api = api;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                return api.post(params[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error";
+            }
+        }
+    }
+}
