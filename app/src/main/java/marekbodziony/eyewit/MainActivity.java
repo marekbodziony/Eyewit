930c71d18c79e4e2873f7310f38a099167055333
Marek Bodziony <marek_bodziony@o2.pl>
2017-10-14 14:21:24 +0200

    Add main app layout

--- app/src/main/java/marekbodziony/eyewit/MainActivity.java
+++ app/src/main/java/marekbodziony/eyewit/MainActivity.java
@@ -2,12 +2,17 @@ package marekbodziony.eyewit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
     }
 }
